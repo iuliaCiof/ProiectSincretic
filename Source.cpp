31b99a362c0dc01e449cef7ded6dbaf925529207
iuliaCiof <iulia.ciofindac@student.upt.ro>
2022-05-04 20:28:30 +0300

    Afisare cifre romane

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,7 @@ int ind_nr_apropiat(int nr){
 	int i;
 
 	for(i=6;i>0;i--){
-		if(nr-numar[i]>0 && nr-numar[i]<nr-numar[i-1]){  //daca diferenta e pozitiva(adica nr e mai mare decat numar[i]) si diferenta este mai mica decat numarul anterior din vectorul numar, atunci acela este numarul cel mai apropiat
+		if(nr-numar[i]>=0 && nr-numar[i]<nr-numar[i-1]){  //daca diferenta e pozitiva(adica nr e mai mare decat numar[i]) si diferenta este mai mica decat numarul anterior din vectorul numar, atunci acela este numarul cel mai apropiat
 			return i;
 		}
 	}
@@ -21,23 +21,36 @@ int main() {
 	int numar[7] = { 1, 5, 10, 50, 100, 500, 1000 };
 	char litere[8] = { 'I', 'V', 'X', 'L', 'C', 'D', 'M', '\0' };
 	int cifre[4];
-	//int ordin = 0;
-	//char N[100];
-	//int n_copy = n;
 	int i=0;
+	int contor=0;
 
+	/*
+	citire N de la tastatura
+
+	*/
 
 	while(N>0){
 		cifre[i] = N%10*pow(10,i); //cifrele se salveaza incepand de la ordinul cel mai mic si se adauga numarul de zerouri corespunzatoare
-		
-		cout<<cifre[i]<<" "<<numar[ind_nr_apropiat(cifre[i])] <<endl;
 		N = N/10;
 		i++;
 	}
 
-	cout<<i;	
 
+	for(int j=i-1;j>=0;j--){
+		int indice = ind_nr_apropiat(cifre[j]);
+		cout<<litere[indice]; //afiseaza cifra romana corespunzatoare
+
+		contor=0;
+		contor = numar[indice]+contor; 
+		
 	
-	
+		while(contor<cifre[j]){ //bug: numerele nu sunt scurtate, de ex: 900 = DCCC si nu CM 
+			indice=ind_nr_apropiat(cifre[j]-contor);
+			contor = numar[indice]+contor;
+			cout<<litere[indice];
+		
+		}
+		cout<<endl;
+	}
 	return 0;
 }
